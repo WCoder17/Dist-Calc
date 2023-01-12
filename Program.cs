5019f5c9bda5bf05ec5b3d1643fb011731f0d9e4
willCodesInBinary <112642024+WCoder17@users.noreply.github.com>
2023-01-12 08:55:16 -0700

    Finished

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Welcome to the Distance Calculator by Will
+#nullable disable
+Console.Clear();
+Console.WriteLine("WELCOME TO THE DISTANCE CALCULATOR");
+
+// Input
+Console.WriteLine("Enter the x1 Value: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter the x2 Value: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter the y1 Value: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter the y2 Value: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+
+// Process
+double dist = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+// Output
+Console.WriteLine($"The Distance is : {dist}");
+
+Console.WriteLine("Thanks For Using the Calculator");
+
